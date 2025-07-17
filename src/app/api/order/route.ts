7d7b87383840cf3441958f1639414eb069c5ff74
dncmaduro <manhdungtran.vnuuet@gmail.com>
2025-07-17 18:28:57 +0700

    feat: pagination

--- src/app/api/order/route.ts
+++ src/app/api/order/route.ts
@@ -4,13 +4,10 @@ import clientPromise from '@/lib/mongodb';
 export async function POST(req: NextRequest) {
   try {
     const data = await req.json();
-
     const client = await clientPromise;
     const db = client.db('data');
     const collection = db.collection('landing-orders');
-
     const result = await collection.insertOne(data);
-
     return NextResponse.json({ success: true, insertedId: result.insertedId });
   } catch (error) {
     return NextResponse.json({ success: false, error: (error as Error).message }, { status: 500 });
@@ -27,12 +24,28 @@ export async function OPTIONS() {
     },
   });
 }
-export async function GET() {
+
+export async function GET(req: NextRequest) {
   try {
+    const { searchParams } = new URL(req.url);
+
+    const page = Number(searchParams.get('page')) || 1;
+    const pageSize = Number(searchParams.get('pageSize')) || 10;
+    const skip = (page - 1) * pageSize;
+
     const client = await clientPromise;
     const db = client.db('data');
     const collection = db.collection('landing-orders');
-    const result = await collection.find({}).toArray();
+
+    const total = await collection.countDocuments({});
+    const data = await collection.find({}).sort({ _id: -1 }).skip(skip).limit(pageSize).toArray();
+
+    const result = {
+      data,
+      total,
+      page,
+      pageSize,
+    };
 
     const response = NextResponse.json(result);
     response.headers.set('Access-Control-Allow-Origin', '*');
